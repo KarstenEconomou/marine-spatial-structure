b640e7375ce5cfa7e3950e815fdbc70683f80be0
KarstenEconomou <economoukarsten@gmail.com>
2022-02-22 16:12:08 -0400

    Added Fixed ParticleType for simulation fetching purposes

--- utils/particletype.py
+++ utils/particletype.py
@@ -1,30 +1,37 @@
 """Defines the ParticleType strategy design pattern."""
 import sys
 from abc import ABC, abstractmethod
-from typing import Dict, Sequence
+from typing import Dict, Sequence, Optional
 from pathlib import Path
 
 import numpy as np
+from netCDF4 import Dataset
 
 sys.path.insert(1, str(Path.cwd() / 'utils'))
 from constants import PLD, CP  # noqa: E402
 from geneticlineage import GeneticLineage  # noqa: E402
 from particle import Particle  # noqa: E402
+from season import Season  # noqa: E402
 from zone import Zone  # noqa: E402
 
 
 class ParticleType(ABC):
     """Abstract base class for a strategy filtering a particle type based on suitable habitat."""
+    @staticmethod
+    @abstractmethod
+    def get_simulation(season: Season) -> Dataset:
+        """Get the simulation file."""
+        pass
 
     @staticmethod
     @abstractmethod
-    def filter_initial_positions(particles: Sequence[Particle], zones: Dict[Zone, GeneticLineage]) -> Sequence[Particle]:
+    def filter_initial_positions(particles: Sequence[Particle], zones: Optional[Dict[Zone, GeneticLineage]] = None) -> Sequence[Particle]:
         """Filter the initial positions of a particle set."""
         pass
 
     @staticmethod
     @abstractmethod
-    def filter_final_positions(particles: Sequence[Particle], zones: Dict[Zone, GeneticLineage]) -> Sequence[Particle]:
+    def filter_final_positions(particles: Sequence[Particle], zones: Optional[Dict[Zone, GeneticLineage]] = None) -> Sequence[Particle]:
         """Filter the final positions of a particle set."""
         pass
 
@@ -34,13 +41,42 @@ class Unrestricted(ParticleType):
     name: str = 'unrestricted'
 
     @staticmethod
-    def filter_initial_positions(particles: Sequence[Particle], zones: Dict[Zone, GeneticLineage]) -> Sequence[Particle]:
+    def get_simulation(season: Season):
+        """Get the simulation file."""
+        return Dataset(Path.cwd() / 'data' / 'simulations' / f'{season.name}.nc')
+
+    @staticmethod
+    def filter_initial_positions(particles: Sequence[Particle]) -> Sequence[Particle]:
         """Filter the initial positions of a particle set."""
         # Unrestricted spawn
         return particles
 
     @staticmethod
-    def filter_final_positions(particles: Sequence[Particle], zones: Dict[Zone, GeneticLineage]) -> Sequence[Particle]:
+    def filter_final_positions(particles: Sequence[Particle]) -> Sequence[Particle]:
+        """Filter the final positions of a particle set."""
+        # Assign final positions at time PLD
+        for particle in particles:
+            particle.final_position = particle.get_position(PLD)
+
+        return particles
+
+class Fixed(ParticleType):
+    """Class of particles that are free to start and end their trajectories anywhere."""
+    name: str = 'fixed'
+
+    @staticmethod
+    def get_simulation(season: Season):
+        """Get the simulation file."""
+        return Dataset(Path.cwd() / 'data' / 'simulations' / f'{season.name}_fixed.nc')
+
+    @staticmethod
+    def filter_initial_positions(particles: Sequence[Particle]) -> Sequence[Particle]:
+        """Filter the initial positions of a particle set."""
+        # Unrestricted spawn
+        return particles
+
+    @staticmethod
+    def filter_final_positions(particles: Sequence[Particle]) -> Sequence[Particle]:
         """Filter the final positions of a particle set."""
         # Assign final positions at time PLD
         for particle in particles:
@@ -53,6 +89,11 @@ class Restricted(ParticleType):
     """Class of particles that are restricted to suitable habitat spawn and settlement."""
     name: str = 'restricted'
 
+    @staticmethod
+    def get_simulation(season: Season):
+        """Get the simulation file."""
+        return Dataset(Path.cwd() / 'data' / 'simulations' / f'{season.name}.nc')
+        
     @staticmethod
     def filter_initial_positions(particles: Sequence[Particle], zones: Dict[Zone, GeneticLineage]) -> Sequence[Particle]:
         """Filter the initial positions of a particle set."""
